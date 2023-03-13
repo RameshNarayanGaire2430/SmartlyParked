e18348d6bdddc1db8b7640c8a76e489e3bfe5ed9
RameshNarayanGaire2430 <n01452430@humbermail.ca>
2023-03-13 12:02:17 -0400

    Different fragments added

---
+++ app/src/main/java/com/smartly/parked/SupportFragment.java
@@ -0,0 +1,49 @@
+package com.smartly.parked;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SupportFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SupportFragment extends Fragment {
+
+    public SupportFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment SupportFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SupportFragment newInstance() {
+        SupportFragment fragment = new SupportFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_support, container, false);
+    }
+}
